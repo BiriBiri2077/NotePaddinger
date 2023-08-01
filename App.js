8abf90bfd947db3e01c59385432c2be389947962
BiriBiri2077 <braxyychantop2019@gmail.com>
2023-08-01 19:54:42 -0300

    Notepad start

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { View, Button, Touchable, TouchableOpacity, StyleSheet } from "react-native"
+
+
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [estado,setarEstado] = useState('leitura');
+  const [anotacao,setarAnotacao] = useState('Minha anotação');
+
+if(estado == 'leitura'){
+ return(
+  <View style={{flex:1}} >
+    <StatusBar style="light" />
+    <View style={styles.header}><Text style={{textAllign:'center', color:'white'}}>Aplicativo Anotação</Text></View>
+    
+    <View style={{padding:20}}><Text>{anotacao}</Text></View>
+    
+    <TouchableOpacity onPress={()=>setarEstado('atualizando')} style={styles.btnAnotacao}><Text style={styles.btnAnotacaoTexto}>+</Text></TouchableOpacity>
+  </View>
+ )
+}else if(estado == 'atualizando'){
+  return(
+  <View style={{flex:1}} >
+    <StatusBar style="light" />
+    <View style={styles.header}><Text style={{textAlign:'center', color:'white'}}>Aplicativo Anotação</Text></View>
+    
+    
+    <TouchableOpacity onPress={()=>setarEstado('leitura')} style={styles.btnSalvar}><Text style={{textAlign:'center', color:'white'}}>Salvar</Text></TouchableOpacity>
+  </View>
+);}
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header:{
+    width: '100%',
+    padding:10,
+    backgroundColor: '#069'
   },
+  anotacao:{
+    fontSize:13
+  },
+  btnAnotacao:{
+    position:'absolute',
+    right:20,
+    bottom:20,
+    width:50,
+    height:50,
+    backgroundColor:'#069',
+    borderRadius:25
+  },
+  btnAnotacaoTexto:{
+    color:'white',
+    position:'relative',
+    left:'50%',
+    textAlign:'center',
+    top:3,
+    fontSize:30
+  },
+  btnSalvar:{
+    position:'absolute',
+    right:20,
+    bottom:20,
+    width:100,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#069',
+    }
+
 });
